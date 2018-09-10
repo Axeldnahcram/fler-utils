aa6eed44fbe30c70d1646b98df71acccf8aea4ec
Axel Marchand <axel.marchand@crossquantum.com>
2018-09-10 15:10:50 +0200

    FLER_UTILS -UPD- update on the directories

--- fler_utils/commons.py
+++ fler_utils/commons.py
@@ -28,17 +28,27 @@ def get_asset_root() -> Dict[str, str]:
     :return: the different assets paths to get sql and csv directories
     :rtype: dictionnary
     """
-    dir = pathlib.Path(__file__).parent
+    dir = pathlib.Path(__file__).parent.parent.parent
     pql_root = str(pathlib.PurePath(dir, 'ext_files/pkl'))
     csv_root = str(pathlib.PurePath(dir, 'ext_files/csv'))
     txt_root = str(pathlib.PurePath(dir, 'ext_files/txt'))
+    gazetteer_en_root = str(pathlib.PurePath(dir, 'ext_files/csv/Gazetteer_ENG'))
+    gazetteer_fr_root = str(pathlib.PurePath(dir, 'ext_files/csv/Gazetteer_FR'))
+    json_root = str(pathlib.PurePath(dir, 'ext_files/json/'))
+    trainingdatasets_root = str(pathlib.PurePath(dir, 'ext_files/trainingdatasets/'))
     dict = {}
+    dict
     dict["pql_root"] = pql_root
     dict["csv_root"] = csv_root
     dict["txt_root"] = txt_root
+    dict["gazetteer_en_root"] = gazetteer_en_root
+    dict["gazetteer_fr_root"] = gazetteer_fr_root
+    dict["json_root"] = json_root
+    dict["trainingdatasets_root"] = trainingdatasets_root
+
     return dict
 
-def get_file_content(cfg, name):
+def get_file_content(cfg, name, gaztype = None):
     contents = {}
     if cst.CSV_ROOT in cfg:
         root_folder = cfg.get(cst.CSV_ROOT)
@@ -55,16 +65,48 @@ def get_file_content(cfg, name):
         txt_file = f"{root_folder}/{name}.txt"
         if os.path.isfile(txt_file):
             return txt_file
-
+    if cst.JSON_ROOT in cfg:
+        root_folder = cfg.get(cst.JSON_ROOT)
+        txt_file = f"{root_folder}/{name}.json"
+        if os.path.isfile(txt_file):
+            return txt_file
+    if cst.TRAININGDATASETS_ROOT in cfg:
+        root_folder = cfg.get(cst.TRAININGDATASETS_ROOT)
+        LOGGER.info(root_folder)
+        txt_file = f"{root_folder}/{name}.csv"
+        LOGGER.info(txt_file)
+        if os.path.isfile(txt_file):
+            return txt_file
+    if name == 'gazetteer_en':
+        root_folder = cfg.get('gazetteer_en_root')
+        ret = []
+        if gaztype is None:
+            for i in os.listdir(root_folder):
+                ret.append(f"{root_folder}/{i}")
         else:
-            LOGGER.error("File named %s does not exist", name)
+            for i in os.listdir(f"{root_folder}/{gaztype}"):
+                ret.append(f"{root_folder}/{gaztype}/{i}")
+        return ret
+
     else:
-        LOGGER.error("sql_root is not defined in configuration")
+        LOGGER.error("File named %s does not exist", name)
+
     return contents
 
+def get_type_of_gazetteers(cfg,language: str='en'):
+    """
+    get the list of the different gazetteers available for a language
+    :param language: name of the language, either en, fr for now
+    :type language: string
+    :return: list of all the different gazetteer
+    :rtype: list
+    """
+    root_folder = cfg.get(f'gazetteer_{language}_root')
+    list_gaz = os.listdir(root_folder)
+    return list_gaz
+
 if __name__ == "__main__":
-    # f = get_asset_root()
-    # LOGGER.info(f)
-    # g = get_file_content(f, "francais")
-    # LOGGER.info(g)
-    LOGGER.info(3)
+    f = get_asset_root()
+    LOGGER.info(f)
+    g = get_file_content(f, "CoNLL2003/test")
+    LOGGER.info(g)
